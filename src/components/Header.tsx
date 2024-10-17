8991edcb2c92b28235fdc2e5d0854e96299896d6
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-10-17 09:27:03 -0300

    feat - Header implematation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+// import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <header>
-        <div>
-          <img src="../src/assets/logo-github.svg" alt="Logo Github Finder" />
-          <h1>GitHub Finder</h1>
-        </div>
+    <header className='Header'>
+      <img src="../src/assets/logo-github.svg" alt="Logo Github Finder" />
+      <h1>GitHub Finder</h1>
     </header>
   )
 }
