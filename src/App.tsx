85b55b9901bf874b2e1fe0e397d749efc9c6df2f
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-10-17 09:28:10 -0300

    feat - App implematation

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@
 import './App.css';
 
 import Header from './components/Header';
-import FormSearchUser from './components/FormSearchUser';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Repository from './pages/Repository';
 
 function App() {
+
   return (
     <div className='App'>
       <Header />
-      <FormSearchUser />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:id/repos' element={<Repository />} />
+        <Route path='*' element={<h1 className='not-found'>Not Found</h1>} />
+      </Routes>
       <Footer /> 
     </div>
   )
