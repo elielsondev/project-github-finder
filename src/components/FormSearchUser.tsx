b28b380e7d17baf6a889068dc2ef343d9e11a420
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-10-17 09:26:24 -0300

    feat - FormSearchUser implematation

--- src/components/FormSearchUser.tsx
+++ src/components/FormSearchUser.tsx
@@ -1,17 +1,47 @@
 // import React from 'react';
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
 
-function FormSearchUser() {
+import "./FormSearchUser.css";
+
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+function FormSearchUser({ loadUser }: SearchProps) {
+  const [userName, setUserName] = useState("");
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      loadUser(userName);
+      setUserName("");
+    }
+  };
+
+  const handleClick = () => {
+    loadUser(userName);
+    setUserName("");
+  };
+
   return (
-    <div>
-      <h1>Busque por um usuário:</h1>
-      <h2>Conheça seus melhores repósitórios</h2>
+    <div className="FormSearchUser">
+      <h2>Busque por um usuário:</h2>
+      <p>Conheça seus melhores repósitórios</p>
 
-      <form action="" method="">
-        <input type="text" placeholder="Digite o nome do usuario" />
-        <button type="submit">Buscar</button>
-      </form>       
+      <div className="search-input-button">
+        <input
+          type="text"
+          placeholder="ex.: elielsondev"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleClick}>
+          <BsSearch />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FormSearchUser
+export default FormSearchUser;
